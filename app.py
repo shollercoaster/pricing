4243e30b7d44a3ec60cfdad98786296998db73e5
saumya <86468578+shollercoaster@users.noreply.github.com>
2023-10-02 17:03:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask, request, jsonify
-from dynamic_pricing import predict
+import dynamic_pricing 
+import joblib
 
 app = Flask(__name__)
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get data from the request
-    data = request.get_json()
 
-    # Use your machine learning model to make predictions
-    prediction = predict(data)  # Replace with your model code
+loaded_rf_model = joblib.load("random_forest_model.pkl")
 
-    # Return the prediction as a JSON response
-    return jsonify({'prediction': prediction})
+@app.route('/', methods=['POST'])
+def predict_amt():
+    try:
+        # Get data from the request
+        data = request.get_json()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        # Use your machine learning model to make predictions
+        prediction = loaded_rf_model.predict(data)  # Replace with your model code
+    
+        # Return the prediction as a JSON response
+        return jsonify({'prediction': prediction})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+if __name__ == "__main__":
+     app.run(host="0.0.0.0", port=5000, debug=True)
